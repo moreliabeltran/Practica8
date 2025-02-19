68190155a8b7c63a9faa2c5cfb0307f77c9d8f5d
moreliabeltran <21130049@e-itesca.edu.mx>
2025-02-19 09:01:15 -0700

    PRACTICA8V1

---
+++ app/src/main/java/mx/edu/itesca/practica8/MainActivity.kt
@@ -0,0 +1,29 @@
+package mx.edu.itesca.practica8
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.botonP)
+
+        button.setOnClickListener() {
+            var intent: Intent = Intent(this,catalogo::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
